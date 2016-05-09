536b3be8bf41d55d7e073824f77df108f8cde350
rmpardee <ruthmpardee@gmail.com>
2016-05-09 16:55:51 -0700

    Progress on blinkyDancer.js

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,24 +1,43 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  makeDancer.call(this);
+var MakeBlinkyDancer = function(top, left, timeBetweenSteps){
 
+  this.oldStep = function(){
+    MakeDancer.step(timeBetweenSteps);
+  };
+  // NOTE, we replaced the line below with line 5
   //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  MakeDancer.call(this, top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  var newStep = this.step;//
 
-  var oldStep = this.step;
-
-  //return blinkyDancer;
 };
 
-makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
+//MakeBlinkyDancer.prototype.constructor = MakeBlinkyDancer;
+MakeBlinkyDancer.prototype.constructor = MakeDancer;
 
-makeBlinkyDancer.prototype.step = function(){
+MakeBlinkyDancer.prototype.step = function(){
+  console.log('blinkyDancer step is running');
   // call the old version of step at the beginning of any call to this new version of step
-  oldStep();
+  this.oldStep(); // NOTE, leaving this in, caused problems so we commented it out.
+
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
   this.$node.toggle();
 };
+
+
+
+// var test = new makeDancer(10,20,100);
+// test.step();
+// console.log(test);
+//var makeBlinkyDancer = makeBlinkyDancer(top, left, timeBetweenSteps);
+
+
+
+// var invokeBlinkyDancer = function(top, left, timeBetweenSteps){
+//   return new makeBlinkyDancer(top, left, timeBetweenSteps);    
+// };
+// invokeBlinkyDancer()
--- src/dancer.js
+++ src/dancer.js
@@ -1,31 +1,30 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps){
-
+var MakeDancer = function(top, left, timeBetweenSteps){
+  // NOTE: maybe save timeBetweenSteps func, for later use
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-
-
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
-  //this.step();
+  this.step(timeBetweenSteps); // update this step function
+
 };
 
-
-
-
-makeDancer.prototype.step = function(){
+MakeDancer.prototype.step = function(timeBetweenSteps){
+  console.log('MakeDancer step is running');
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
+  // NOTE, may need to use a complete function inside of setTimeout as below:
   setTimeout(function(){return this.step }, timeBetweenSteps);
-  this.step();
+  //setTimeout(this.step(), timeBetweenSteps);
+
 };
 
-makeDancer.prototype.setPosition = function(top, left){
+MakeDancer.prototype.setPosition = function(top, left){
+  console.log('setPosition');
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
-  //
   var styleSettings = {
     top: top,
     left: left
